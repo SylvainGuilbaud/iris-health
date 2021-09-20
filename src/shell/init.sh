015f8ad02f2dfbde8378acabd0528320e1d4df01
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2021-09-20 17:46:06 +0200

    init

--- src/shell/init.sh
+++ src/shell/init.sh
@@ -4,6 +4,11 @@ iris session $ISC_PACKAGE_INSTANCENAME -U %SYS << END
 write ##class(Security.Users).UnExpireUserPasswords("*")
 do \$SYSTEM.OBJ.Load("/src/app/install.cls", "ck") 
 set sc = ##class(app.install).init() 
+zn "app"
+set recurse = 1
+set dir = "/src/cls"
+do \$system.OBJ.LoadDir(dir,"ck",.errors,1)
+merge ^INIT.ERRORS(dir)=errors
 halt
 END
 exit=$?
