49ef133251077b4b7477189ad05f3bfd9c30ea23
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2021-09-21 18:45:40 +0200

    init

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ ARG IMAGE=containers.intersystems.com/intersystems/irishealth-community:2021.1.0
 FROM $IMAGE
 USER root
 
-RUN mkdir /data && chown irisowner:irisowner /data
+RUN mkdir /data /databases && chown irisowner:irisowner /data /databases
 
 USER irisowner
 COPY ./src /tmp/src
--- src/shell/init.sh
+++ src/shell/init.sh
@@ -2,13 +2,12 @@
 
 iris session $ISC_PACKAGE_INSTANCENAME -U %SYS << END
 write ##class(Security.Users).UnExpireUserPasswords("*")
-do \$SYSTEM.OBJ.Load("/src/app/install.cls", "ck") 
-set sc = ##class(app.install).init() 
-zn "app"
-set recurse = 1
-set dir = "/src/cls"
-do \$system.OBJ.LoadDir(dir,"ck",.errors,1)
-merge ^INIT.ERRORS(dir)=errors
+do \$SYSTEM.OBJ.Load("/src/cls/init/install.cls", "ck") 
+set sc = ##class(init.install).init() 
+zn "interop"
+write ##class(Ens.Director).SetAutoStart("INTEROP.production")
+write ##class(Ens.Director).StartProduction("INTEROP.production")
+write ##class(Ens.Config.Credentials).SetCredential("IRIS","_system","SYS",1)
 halt
 END
 exit=$?
